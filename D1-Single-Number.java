3e1f63be0d9ed32ebe9ab905d4a13b87f6e7fea9
3LexW <52439478+3LexW@users.noreply.github.com>
2020-04-01 22:45:14 +0800

    Delete D1-Single-Number.java
    File organization

--- D1-Single-Number.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int n : nums) ans = ans ^ n; // ^ is the bitwise operator of XOR, if two numbers (n) are the same, n ^ n = 0
-        return ans;
-    }
-}
