04b442e31c1962a0c45674660065e46d7e0f8fa3
3LexW <52439478+3LexW@users.noreply.github.com>
2020-05-02 18:59:08 +0800

    Create D2-Jewels-and-Stones.java

---
+++ java-code-May-2020/D2-Jewels-and-Stones.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        HashSet<Character> jewels = new HashSet<>();
+        for (char c: J.toCharArray()) jewels.add(c);
+        
+        int count = 0;
+        for (char c: S.toCharArray()) if (jewels.contains(c)) count++;
+        return count;
+    }
+}
