ca259209349522b3f966b10c0cbbe05b52f5e07f
3LexW <52439478+3LexW@users.noreply.github.com>
2020-05-18 17:48:23 +0800

    Update D18-Permutation-in-String.java

--- java-code-May-2020/D18-Permutation-in-String.java
+++ java-code-May-2020/D18-Permutation-in-String.java
@@ -21,3 +21,24 @@ class Solution {
         return true;
     }
 }
+
+//Method 2: Same as D17's solution
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        //Using a sliding window to see if the part of the s2 and s1 are anagrams
+        int[] count = new int[26];
+        for (char c: s1.toCharArray()) count[c-'a']++;
+        
+        int left = 0;
+        int right = 0;
+        int total = s1.length();    //If total reaches 0, permutation exists
+        
+        while (right < s2.length()){
+            if (count[s2.charAt(right++) - 'a']-- >= 1) total--;
+            if (total == 0) return true; //Found anagram
+            if (right - left == s1.length() && count[s2.charAt(left++) - 'a']++ >= 0) total++;
+        }
+        
+        return false;
+    }
+}
